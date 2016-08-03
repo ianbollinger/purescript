9ef4550ec813fa8dc6a7d3a5152a1f700dc1e14d
Ian D. Bollinger <ian.bollinger@gmail.com>
2016-08-02 20:12:28 -0400

    Correctly pretty-print block comments

--- psc-format/Comments.hs
+++ psc-format/Comments.hs
@@ -8,6 +8,6 @@ import Language.PureScript.Comments
 
 instance Pretty Comment where
     pretty (LineComment s) = text "--" <> pretty s
-    pretty (BlockComment s) = text "--" <> pretty s
+    pretty (BlockComment s) = text "{-" <> pretty s <> text "-}"
 
     prettyList = vcat . fmap pretty
